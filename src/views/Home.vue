d716a9ba62aa57b4030686321e34e01298088bd8
xiaowei-guo <18131093042@163.com>
2018-08-11 19:38:51 +0800

    用户列表默认不展开

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,7 +24,6 @@
         router 是否使用 vue-router 的模式，启用该模式会在激活导航时以 index 作为 path 进行路由跳转
         -->
         <el-menu
-          default-active="user"
           unique-opened
           router
           class="aside-menu">
