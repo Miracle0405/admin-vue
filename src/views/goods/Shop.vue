b1a5b08af183136b2a82578ac6861641550aac71
xiaowei-guo <18131093042@163.com>
2018-08-13 21:51:15 +0800

    商品分类表格数据的渲染

--- src/views/goods/Shop.vue
+++ src/views/goods/Shop.vue
@@ -1,14 +1,90 @@
 <template>
-  <div>
-    shop
-  </div>
+  <el-card>
+    <!-- 面包屑组件 -->
+    <my-bread level1="商品管理" level2="商品分类"></my-bread>
+    <!-- 按钮 -->
+    <el-row class="row">
+      <el-col :span="24">
+        <el-button type="success" plain>添加分类</el-button>
+      </el-col>
+    </el-row>
+
+    <!-- 表格 -->
+    <el-table
+      :data="data"
+      style="width: 100%"
+      border
+      stripe>
+      <el-table-column
+        prop="cat_name"
+        label="分类名称"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        label="级别"
+        width="180">
+        <template slot-scope="scope">
+          <span v-if="scope.row.cat_level === 0">一级</span>
+          <span v-if="scope.row.cat_level === 1">二级</span>
+          <span v-if="scope.row.cat_level === 2">三级</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="是否有效">
+        <template slot-scope="scope">
+          {{ scope.row.cat_deleted === false ? '无效' : '有效' }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="操作">
+        <template slot-scope="scope">
+          <el-button
+            plain
+            size="mini"
+            type="primary"
+            icon="el-icon-edit">
+          </el-button>
+          <el-button
+            plain
+            size="mini"
+            type="primary"
+            icon="el-icon-delete">
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      data: []
+    };
+  },
+  created() {
+    this.loadData();
+  },
+  methods: {
+    async loadData() {
+      const response = await this.$http.get('categories?type=3');
+      console.log(response);
+      const { meta: { msg, status } } = response.data;
+      if (status === 200) {
+        this.data = response.data.data;
+      } else {
+        this.$message.error(msg);
+      }
+    }
+  }
 };
 </script>
 
 <style>
-
+.row {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
 </style>
